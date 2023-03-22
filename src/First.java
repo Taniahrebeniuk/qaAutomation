c127c3aa7587e3fd0293f77f8602d348b7a308c1
Tetiana Hrebeniuk <tetianahr@KIE-X-L-0581.pt.playtech.corp>
2023-03-22 12:49:27 +0200

    Homework1

--- src/First.java
+++ src/First.java
@@ -1,10 +1,13 @@
 public class First {
-        int year = 2023;
-        public static int sumOfTwoNumbets (int a, int b) {
-            int sum = a + b;
-            return sum;}
-        public static void main(String[] args) {
+    int year = 2023;
 
-            sumOfTwoNumbets(10,20);
-        }
+    public static int sumOfTwoNumbets(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        sumOfTwoNumbets(10, 20);
+    }
 }
