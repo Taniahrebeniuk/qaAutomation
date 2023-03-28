a577fc9c9d82872244f43455bd511f77cef80652
Tetianahebeniuk <taniahrebeniuk@gmail.com>
2023-03-28 22:41:01 +0300

    Homework2 update

--- src/TestOverloading1.java
+++ src/TestOverloading1.java
@@ -1,36 +1,37 @@
 import java.util.Scanner;
 
 public class TestOverloading1 {
-
-    static void testParameters(float a, float b, float c) {
-        float i = a + b + c;
-        System.out.println(i);
+    static void testParameters(int input) {
+        System.out.println("The sum of each number for " + input + " is: " +
+                ((input % 10) + ((input / 10) % 10) + ((input / 100) % 10)));
     }
 
     static void testParameters(String str1, String str2) {
         int numberOfLetter = str1.length() + str2.length();
         System.out.println("String Length \"Hello\": " + str1.length() + " symbols");
         System.out.println("String Length \"World!\": " + str2.length() + " symbols");
-
     }
 
-    static void testParameters(int value) {
+    static boolean testParameters() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Add some value");
+        System.out.println("Add some number");
         int sum = sc.nextInt();
+
         if (sum > 0) {
-            System.out.println("true");
-        } else if (sum <= 0) {
-            System.out.println("false");
+            System.out.println("Number is greater than zero");
+
+            return true;
+
+        } else {
+            System.out.println("Number is not greater than zero");
+
         }
-
+        return false;
     }
-
 
     public static void main(String[] args) {
-        testParameters(5, 10, 15);
-        testParameters("Hello", "World!");
-        testParameters(1);
+        testParameters(200);
+        testParameters(0);
+        testParameters();
     }
-
 }
