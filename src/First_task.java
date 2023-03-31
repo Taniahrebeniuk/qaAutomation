4e77bfe88911a72dbe476525a08e60d4c0cb9e63
Tetianahebeniuk <taniahrebeniuk@gmail.com>
2023-03-31 17:27:50 +0300

    Homework3

---
+++ src/First_task.java
@@ -0,0 +1,22 @@
+public class First_task {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "Hi";
+        String c = "Greetings";
+
+        if (a.length() > b.length() && a.length() > c.length()) {
+            System.out.println(a + " word is the largest");
+        } else {
+            if (b.length() > a.length() && b.length() > c.length()) {
+                System.out.println(b + " word is the largest");
+            } else {
+                if (c.length() > a.length() && c.length() > b.length()) {
+                    System.out.println(c + " word is the largest");
+                } else {
+                    System.out.println("Another word is the largest one");
+                }
+            }
+        }
+    }
+}
+
---
+++ src/Star_pattern1.java
@@ -0,0 +1,20 @@
+public class Star_pattern1 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 4; i++) {
+            for (int j = i; j >= 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int m = 4; m >= 1; m--) {
+            for (int n = m; n >= 1; n--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
