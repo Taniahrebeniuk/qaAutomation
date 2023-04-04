dd00f490c4a5f00dd1871a5850deb5201e1877f1
Tetianahebeniuk <taniahrebeniuk@gmail.com>
2023-04-04 12:34:03 +0300

    Homework3 update

--- src/Month.java
+++ src/Month.java
@@ -1,46 +1,31 @@
 public class Month {
-    static void monthNumber(int number) {
+    static void monthDaysCounter(int monthNumber) {
 
-        switch (number) {
+        switch (monthNumber) {
             case 1:
-                System.out.println("January - 31 days");
-                break;
-            case 2:
-                System.out.println("February - 28 days");
-                break;
             case 3:
-                System.out.println("March - 31 days");
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(monthNumber + " month of the 2023 Year contains 31 days");
                 break;
             case 4:
-                System.out.println("April - 30 days");
-                break;
-            case 5:
-                System.out.println("May - 28 days");
-                break;
             case 6:
-                System.out.println("June - 30 days");
-                break;
-            case 7:
-                System.out.println("July - 31 days");
-                break;
-            case 8:
-                System.out.println("August - 31 days");
-                break;
             case 9:
-                System.out.println("September - 30 days");
-                break;
-            case 10:
-                System.out.println("October - 31 days");
-                break;
             case 11:
-                System.out.println("November - 30 days");
+                System.out.println(monthNumber + " month of the 2023 Year contains 30 days");
                 break;
-            case 12:
-                System.out.println("December - 31 days");
+            case 2:
+                System.out.println(monthNumber + " month of the 2023 Year contains 28 days");
                 break;
+            default:
+                System.out.println("Sorry, you are not right, we have only 12 months in a year");
         }
+
     }
-    public static void main(String[] args) {
+
+    private static void monthNumber(int i) {
         monthNumber(3);
     }
 }
